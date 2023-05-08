4e56f886f6fc0298c4480ad19104a90ce4dc9d10
martscastrillo <mart.castrillo@gmail.com>
2023-05-08 18:04:34 +0200

    add files

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: martcast <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 18:00:11 by martcast          #+#    #+#             */
+/*   Updated: 2023/05/08 18:00:14 by martcast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <string.h>
+# include <stdio.h>
+# include <unistd.h> 
+# include <stdlib.h>
+
+char    *get_next_line(int fd);
+#endif
